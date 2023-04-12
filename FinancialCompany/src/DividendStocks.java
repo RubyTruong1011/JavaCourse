e15bd058ad7709a9ca0f73bd573bc8d840b64e8d
Ruby Truong <rubytruong1011@gmail.com>
2023-04-12 18:34:59 -0400

    Financial Company

---
+++ FinancialCompany/src/DividendStocks.java
@@ -0,0 +1,6 @@
+public class DividendStocks extends Stocks {
+
+
+
+}
+
---
+++ FinancialCompany/src/Stocks.java
@@ -0,0 +1,12 @@
+public class Stocks implements Asset{
+
+    @Override
+    public double getMarketValue() {
+        return 0;
+    }
+
+    @Override
+    public double getProfit() {
+        return 0;
+    }
+}
