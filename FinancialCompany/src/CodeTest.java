92fe3c102d945b3dabb8371afdc79c47759c361a
Ruby Truong <rubytruong1011@gmail.com>
2023-04-12 18:34:44 -0400

    Financial Company

---
+++ FinancialCompany/src/CodeTest.java
@@ -0,0 +1,22 @@
+public class CodeTest {
+    public static void main(String[] args) {
+//        Case Study: Financial Software
+
+        /**
+         * let's consider the problem of gathering info about a person's financial investments.
+         * we have Stocks, Dividend Stocks, Mutual Funds, Real Estate, Cash ...
+         *
+         * How would you design a complete portfolio system?
+         * What new types of objects would you write?
+         *
+         */
+
+//        1- CLasses: Stocks, DividendSticks, MutualFunds, Cash
+//        2- Relations: DevidendStock is-a Stock
+//        3- Because different assets compute their market value in different ways:
+//        We use Interface: getMarketValue and getProfit
+//        4- Similarities between mutual funds and stocks??
+//        both store assets that are based on shares
+
+    }
+}
